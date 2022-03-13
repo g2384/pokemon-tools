c7742d6fa6e960920ed568087423edeaad6e520b
g2384 <g2384@users.noreply.github.com>
2022-03-13 10:38:40 +0000

    add type-calculator; need to consider nature

---
+++ js/typeEffectiveness.js
@@ -0,0 +1,93 @@
+const pokemonTypes = [
+    "Normal",
+    "Fighting",
+    "Flying",
+    "Poison",
+    "Ground",
+    "Rock",
+    "Bug",
+    "Ghost",
+    "Steel",
+    "Fire",
+    "Water",
+    "Grass",
+    "Electric",
+    "Psychic",
+    "Ice",
+    "Dragon",
+    "Dark",
+    "Fairy",
+    "None"
+];
+
+const typeEffectivenessTable = [
+    [1, 1, 1, 1, 1, 0.5, 1, 0, 0.5, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [2, 1, 0.5, 0.5, 1, 2, 0.5, 0, 2, 1, 1, 1, 1, 0.5, 2, 1, 2, 0.5],
+    [1, 2, 1, 1, 1, 0.5, 2, 1, 0.5, 1, 1, 2, 0.5, 1, 1, 1, 1, 1],
+    [1, 1, 1, 0.5, 0.5, 0.5, 1, 0.5, 0, 1, 1, 2, 1, 1, 1, 1, 1, 2],
+    [1, 1, 0, 2, 1, 2, 0.5, 1, 2, 2, 1, 0.5, 2, 1, 1, 1, 1, 1],
+    [1, 0.5, 2, 1, 0.5, 1, 2, 1, 0.5, 2, 1, 1, 1, 1, 2, 1, 1, 1],
+    [1, 0.5, 0.5, 0.5, 1, 1, 1, 0.5, 0.5, 0.5, 1, 2, 1, 2, 1, 1, 2, 0.5],
+    [0, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 0.5, 1],
+    [1, 1, 1, 1, 1, 2, 1, 1, 0.5, 0.5, 0.5, 1, 0.5, 1, 2, 1, 1, 2],
+    [1, 1, 1, 1, 1, 0.5, 2, 1, 2, 0.5, 0.5, 2, 1, 1, 2, 0.5, 1, 1],
+    [1, 1, 1, 1, 2, 2, 1, 1, 1, 2, 0.5, 0.5, 1, 1, 1, 0.5, 1, 1],
+    [1, 1, 0.5, 0.5, 2, 2, 0.5, 1, 0.5, 0.5, 2, 0.5, 1, 1, 1, 0.5, 1, 1],
+    [1, 1, 2, 1, 0, 1, 1, 1, 1, 1, 2, 0.5, 0.5, 1, 1, 0.5, 1, 1],
+    [1, 2, 1, 2, 1, 1, 1, 1, 0.5, 1, 1, 1, 1, 0.5, 1, 1, 0, 1],
+    [1, 1, 2, 1, 2, 1, 1, 1, 0.5, 0.5, 0.5, 2, 1, 1, 0.5, 2, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 0.5, 1, 1, 1, 1, 1, 1, 2, 1, 0],
+    [1, 0.5, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 0.5, 0.5],
+    [1, 2, 1, 0.5, 1, 1, 1, 1, 0.5, 0.5, 1, 1, 1, 1, 1, 2, 2, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1] // none
+];
+
+function calcEffectiveness(type1, type2, return4Levels = false) {
+    if (type2 == undefined || type2 == null) {
+        type2 = "None"
+    }
+    var t1Index = pokemonTypes.indexOf(type1);
+    var t2Index = pokemonTypes.indexOf(type2);
+    if (t1Index < 0) {
+        console.log("failed t1Index " + t1Index);
+        return {};
+    } if (t2Index < 0) {
+        console.log("failed t2Index " + t2Index);
+        return {};
+    }
+    var result = {
+        "0": [],
+        "0.25": [],
+        "0.5": [],
+        "1": [],
+        "2": [],
+        "4": [],
+    };
+    if (return4Levels) {
+        result = {
+            "0": [],
+            "0.5": [],
+            "1": [],
+            "2": [],
+        };
+    }
+    for (var i = 0; i < pokemonTypes.length - 1; i++) {
+        var t1v2 = typeEffectivenessTable[i][t1Index];
+        var t2v2 = typeEffectivenessTable[i][t2Index];
+        if (t2v2 == undefined) {
+            t2v2 = 1;
+        }
+        var v = Math.round(t1v2 * t2v2 * 100) / 100;
+
+        if (return4Levels) {
+            if (v == 0.25) {
+                v = 0.5;
+            }
+            else if (v == 4) {
+                v = 2
+            }
+        }
+        result[v.toString()].push(pokemonTypes[i]);
+    }
+    return result;
+}
